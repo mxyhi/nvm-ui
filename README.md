c315cd08c34e8ecc2f52a75d48a5565902043840
mxy <imxyi@hotmail.com>
2023-01-05 21:06:42 +0800

    up readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # nvm 可视化界面 | nvm visual interface
 
+- 安装版本
+- 卸载版本
+- 切换版本
+- 换源
+
 ![display](./display.gif)
+![display](./replace-mirror.png)
--- components.d.ts
+++ components.d.ts
@@ -17,15 +17,7 @@ declare module '@vue/runtime-core' {
     AListItem: typeof import('@arco-design/web-vue')['ListItem']
     AListItemMeta: typeof import('@arco-design/web-vue')['ListItemMeta']
     ASpace: typeof import('@arco-design/web-vue')['Space']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
